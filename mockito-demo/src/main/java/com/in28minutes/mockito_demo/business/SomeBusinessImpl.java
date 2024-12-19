26ffa3aa7a831ac083a660cecc09cb1eecd67b52
Fakhriddin Mullagoziev <mullagoziev@gmail.com>
2024-12-19 10:55:01 -0500

    Mockito Demo, getting ready basic code

---
+++ mockito-demo/src/main/java/com/in28minutes/mockito_demo/business/SomeBusinessImpl.java
@@ -0,0 +1,24 @@
+package com.in28minutes.mockito_demo.business;
+
+import java.util.Iterator;
+
+public class SomeBusinessImpl {
+	private DataService dataService;
+	
+	public int findGreatestFromAllData() {
+		int[] data=dataService.retrieveAllData();
+		int greatestValue=Integer.MIN_VALUE;
+		for(int value:data) {
+			if (value>greatestValue) {
+				greatestValue=value;
+			}
+		}
+		return greatestValue;
+		
+	}
+}
+
+
+interface DataService{
+	int[] retrieveAllData();
+}
